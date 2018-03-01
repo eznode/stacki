838d4bd4c2b8b5039f7eb88fb0b6ad7e9b59680d
Taylor Sanchez <taylor.sanchez@teradata.com>
2018-03-01 12:06:18 -0800

    I keep forgetting these false and true are strings, I should probably change that... :anguished:

--- sles/src/stack/images/common/sles-stacki.img-patches/opt/stack/bin/output-partition.py
+++ sles/src/stack/images/common/sles-stacki.img-patches/opt/stack/bin/output-partition.py
@@ -113,7 +113,7 @@ def outputPartition(p, initialize):
 		xml_partitions.append('\t\t\t\t<format config:type="boolean">%s</format>' % format)
 		if attributes['disklabel'].lower() != "msdos":
 			xml_partitions.append('\t\t\t\t<filesystem config:type="symbol">%s</filesystem>' % p['fstype'])
-		if not initialize and \
+		if initialize.lower() != "true" and \
 			attributes['os.version'] == "12.x" and \
 			attributes['disklabel'].lower() != "msdos":
 			xml_partitions.append('\t\t\t\t<partition_nr config:type="integer">%s</partition_nr>' % p['partnumber'])
